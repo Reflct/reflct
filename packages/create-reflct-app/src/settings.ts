44835e99120c26097c26eab173202250d016ff2a
ryumy97 <inha.ryu.97@gmail.com>
2025-12-12 15:25:23 +1300

    Remove create react package

--- packages/create-reflct-app/src/settings.ts
+++
@@ -1,8 +0,0 @@
-export const packageMenager = "npm";
-
-export const getGlobalInstallCommand = (packageName: string) =>
-  `${packageMenager} i -g ${packageName}`;
-
-export const defaultValues = {
-  projectName: "my-app",
-};
